48f79e32bde8efb76da354147be0a17e74eefffa
UTI308 <UTI308@sio.local>
2023-09-29 15:11:25 +0200

    Ajout du proto 0

---
+++ proto_0/src/control/DatabaseConnection.java
@@ -0,0 +1,38 @@
+package control;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                String url = "jdbc:postgresql://localhost:5432/QuizGame";
+                String utilisateur = "postgres";
+                String motDePasse = "P@ssw0rdsio";
+
+                Class.forName("org.postgresql.Driver");
+
+                connection = DriverManager.getConnection(url, utilisateur, motDePasse);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
